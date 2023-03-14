32b28bec24d8592745d09f4741c5e4f3acede1ba
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2023-03-14 04:34:01 +0300

    Create AimKind2.java

---
+++ AimKind2.java
@@ -0,0 +1,10 @@
+
+
+public class AimKind2 extends Aim{
+
+    public AimKind2(int duration, String deadline, String subject, String responsible) {
+        super(duration, deadline, subject, responsible);
+        
+    }
+    
+}
