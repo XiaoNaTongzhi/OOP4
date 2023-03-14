8448a70b519a278fc2d3fe28f6e24f82bbf056b3
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2023-03-14 04:32:49 +0300

    Create Calendar.java

---
+++ Calendar.java
@@ -0,0 +1,23 @@
+package j00;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calendar<K> {
+    
+    private List<K> days = new ArrayList<>();
+    
+
+    public Calendar(List<K> days) {
+        this.days = days;
+    }
+
+    public void AddDay(K someDay) {
+        days.add(someDay);
+    }
+
+    public void RemoveDay(K someDay) {
+        days.remove(someDay);
+    }
+
+}
